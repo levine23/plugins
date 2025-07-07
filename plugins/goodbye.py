2c5096a9f6793bae076879ef7886842ae13fcd13
levine23 <annacantik461@gmail.com>
2025-07-08 05:16:06 +0900

    Update goodbye.py

--- plugins/goodbye.py
+++ plugins/goodbye.py
@@ -11,10 +11,10 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     Message,
 )
-from DanteMusic import app
-from DanteMusic.misc import SUDOERS
+from ZeebMusic import app
+from ZeebMusic.misc import SUDOERS
 from .notes import extract_urls
-from DanteMusic.utils.database import is_gbanned_user
+from ZeebMusic.utils.database import is_gbanned_user
 from utils import (
     del_goodbye,
     get_goodbye,
@@ -24,9 +24,9 @@ from utils import (
     set_greetings_off,
 )
 from utils.error import capture_err
-from DanteMusic.utils.functions import check_format, extract_text_and_keyb
-from DanteMusic.utils.keyboard import ikb
-from DanteMusic.utils.permissions import adminsOnly
+from ZeebMusic.utils.functions import check_format, extract_text_and_keyb
+from ZeebMusic.utils.keyboard import ikb
+from ZeebMusic.utils.permissions import adminsOnly
 
 
 async def handle_left_member(member, chat):
