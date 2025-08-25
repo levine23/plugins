637702f49600822dda23bdeeeaa2f6441d4740ce
Levine <annacantik461@gmail.com>
2025-08-26 04:35:54 +0900

    Update figlet.py

--- plugins/figlet.py
+++ plugins/figlet.py
@@ -6,7 +6,7 @@ import pyfiglet
 from pyrogram import filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from ZeebMusic import app
+from damMusic import app
 
 
 def figle(text):
